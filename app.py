ef5a8fda318e1525ca49d90c64d80bf8ac67b3ee
Evgeniy Tregubenko <111185290+evgeniytregubenko@users.noreply.github.com>
2025-06-29 23:24:39 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template_string
+from parser import parse_sportsdirect
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "SportsDirect Monitor is working!"
+    items = parse_sportsdirect()
+    html = "<h2>Тест парсера SportsDirect</h2><ul>"
+    for item in items:
+        html += f"<li><a href='{item['url']}'>{item['name']}</a></li>"
+    html += "</ul>"
+    return html
