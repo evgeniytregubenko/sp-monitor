995f28cb623bac4a12a8e0135c68433038187087
Evgeniy Tregubenko <111185290+evgeniytregubenko@users.noreply.github.com>
2025-06-29 23:45:13 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template_string, redirect, url_for
+import parser
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "✅ Приложение запущено! Парсер пока отключен из-за таймаута."
+    html = """
+    <h2>✅ Приложение запущено!</h2>
+    <a href="/run-parser">
+        <button style="padding:10px 20px; font-size:16px;">Запустить парсер</button>
+    </a>
+    """
+    return render_template_string(html)
+
+@app.route("/run-parser")
+def run_parser():
+    parser.init_db()
+    parser.parse_sportsdirect()
+    return redirect(url_for('home'))
