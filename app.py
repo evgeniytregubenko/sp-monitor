1b7abf3ab1b78aad26ba8d69ce4e779f2c47f087
Evgeniy Tregubenko <111185290+evgeniytregubenko@users.noreply.github.com>
2025-06-29 23:35:41 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,6 @@
-from flask import Flask, render_template_string
-from parser import parse_sportsdirect
-
+from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    items = parse_sportsdirect()
-    html = "<h2>Тест парсера SportsDirect</h2><ul>"
-    for item in items:
-        html += f"<li><a href='{item['url']}'>{item['name']}</a></li>"
-    html += "</ul>"
-    return html
+    return "✅ Приложение запущено! Парсер пока отключен из-за таймаута."
